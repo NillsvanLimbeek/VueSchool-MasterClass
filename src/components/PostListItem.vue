797f18d6c8abc27e649a7dc03f478a609b7e8e72
Sercasaurus <nills.van.limbeek@live.nl>
2018-05-08 21:08:38 +0200

    more components

---
+++ src/components/PostListItem.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="post-list">
+        <div class="post">
+            
+            <div class="user-info">
+                <a href="#" class="user-name">{{ user.name }}</a>
+
+                <a href="#">
+                    <img class="avatar-large" :src="user.avatar" alt="Avatar">
+                </a>
+
+                <p class="desktop-only text-small">{{ userPostCount }} posts</p>
+            </div>
+
+            <div class="post-content">
+                <div>
+                    <p>{{ post.text }}</p>
+                </div>
+            </div>
+
+            <div class="post-date text-faded">
+                {{ post.publisedAt }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import sourceData from '@/data.json';
+
+    export default {
+        computed: {
+            user () {
+                return sourceData.users[this.post.userId];
+            },
+            userPostCount () {
+                return Object.keys(this.user.posts).length;
+            }
+        },
+        props: {
+            post: {
+                required: true,
+                type: Object
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -1,47 +1,34 @@
 <template>
     <div class="col-large push-top">
-            <h1>{{ thread.title }}</h1>
-
-            <div class="post-list">
-                <div class="post"
-                     v-for="postId in thread.posts"
-                     :key="postId.id">
-                    
-                    <div class="user-info">
-                        <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
-
-                        <a href="#">
-                            <img class="avatar-large" :src="users[posts[postId].userId].avatar" alt="Avatar">
-                        </a>
-
-                        <p class="desktop-only text-small">107 posts</p>
-                    </div>
-
-                    <div class="post-content">
-                        <div>
-                            <p>{{ posts[postId].text }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <h1>{{ thread.title }}</h1>
+        <PostList :posts="posts"/>
+    </div>
 </template>
 
 <script>
     import sourceData from '@/data.json';
 
+    import PostList from '@/components/PostList'
+
     export default {
-        props: {
-            id: {
-                required: true,
-                type: String,
-            }
-        },
         data() {
             return {
-                thread: sourceData.threads[this.id],
-                posts: sourceData.posts,
-                users: sourceData.users,
+                thread: sourceData.threads[this.id]
+            }
+        },
+        computed: {
+            posts () {
+                const postdIds = Object.values(this.thread.posts);
+                return Object.values(sourceData.posts)
+                    .filter(post => postdIds.includes(post['.key']));
+            }
+        },
+        components: {
+            PostList
+        },
+        props: {
+            id: {
+                required: true
             }
         }
     }
